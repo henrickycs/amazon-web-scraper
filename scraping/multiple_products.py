6c1adb4289aa230895b6233b7088c6b1d078469b
Henricky Serrao <henrickyserrao@gmail.com>
2025-08-18 12:36:49 -0300

    Adaptar coletor para salvar no banco

--- scraping/multiple_products.py
+++ scraping/multiple_products.py
@@ -2,8 +2,10 @@ import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
 import time
-
+import sqlite3
 import pandas as pd
+
+from database.insert_price import salvar_preco
 
 urls = {
     "DualSense Branco":"https://www.amazon.com.br/PlayStation-DualSense-Controle-sem-fio/dp/B0CQKLS4RP/?_encoding=UTF8&pd_rd_w=7f3us&content-id=amzn1.sym.8fbb3d34-c3f1-46af-9d99-fd6986f6ec8f&pf_rd_p=8fbb3d34-c3f1-46af-9d99-fd6986f6ec8f&pf_rd_r=CJQN0Q9D6ZWXSD51E082&pd_rd_wg=oCv2M&pd_rd_r=01162b1c-a725-45c5-ad33-10c7d3e50f75&ref_=pd_hp_d_btf_crs_zg_bs_7791985011&th=1",
@@ -17,36 +19,27 @@ headers = {
 }
 
 def coletar_preco(nome_produto, url):
-    try:
-        response = requests.get(url, headers=headers)
-        soup = BeautifulSoup(response.content, "html.parser")
+    response = requests.get(url, headers=headers, timeout=10)
+    soup = BeautifulSoup(response.content, "html.parser")
 
-        titulo = soup.find(id = "productTitle")
-        titulo = titulo.get_text().strip() if titulo else "Título não encontrado"
+    titulo = soup.find(id = "productTitle")
+    titulo = titulo.get_text().strip() if titulo else nome_produto
 
-        preco = soup.find("span", class_ ="a-offscreen")
-        preco = preco.get_text().strip() if preco else "Preço não encontrado"
+    preco = soup.find("span", class_ ="a-offscreen")
+    preco = preco.get_text().strip() if preco else None
 
-        return{
-            "produto": nome_produto,
-            "preco": preco,
-            "data": datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        }
-    except Exception as e:
-        return{
-            "produto": nome_produto,
-            "preco": None,
-            "data": datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-            "erro": str(e)
-        }
+    data = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-resultados = []
+    return titulo, preco, data
+
+
 for nome, url in urls.items():
-    info = coletar_preco(nome,url)
-    resultados.append(info)
-    print(info)
+    titulo, preco, data =  coletar_preco(nome, url)
+    salvar_preco(titulo, preco, data)
+    print(f"Salvo: {titulo} | {preco} | {data}")
     time.sleep(2)
 
-df = pd.DataFrame(resultados)
-print("\nColeta final:")
-print(df)
+conn = sqlite3.connect("precos.db")
+df = pd. read_sql("SELECT * FROM precos", conn)
+print(df.head())
+conn.close()
